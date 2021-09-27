9ee6cf53b7134e8e0ccb7f2d5c40294b953814fc
shivank <68537459+sh712-12@users.noreply.github.com>
2021-09-27 12:34:26 +0300

    patterns

---
+++ patterns.py
@@ -0,0 +1,41 @@
+'''
+#   1           [1,2)
+#   2 3         [2,4)
+#   4 5 6       [4,7)
+
+n=int(input('enter a number: '))
+t=1
+for i in range(1, n):
+  for j in range(1, i+1):
+    print(t, end=' ')
+    t+=1
+  print()
+'''
+
+'''
+#    1
+#  2   2
+#3   3  3
+
+n=int(input('enter a number: '))
+for i in range(1,n):
+  print(' '*(n-i), end=' ')
+  for j in range(1, i+1):
+    print(i, end=' ')
+  print()
+'''
+
+'''
+#   A
+#  B B
+# C C C
+
+l = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+n=int(input('enter a number: '))
+print(l)
+for i in range(0, n):
+  print(' '*(n-i), end=' ')
+  for j in range(0, i+1):
+    print(l[i], end=' ')
+  print()
+'''
